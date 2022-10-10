f5af60eeb0a3f6b2ffba27034bff149698f7c309
Lin Pei Wen <mobilesuit0922@gmail.com>
2022-10-10 21:56:24 +0800

    feat: 好了，明天繼續搞

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
+const path = require("path");
+
 const jestConfig = {
+  rootDir: path.join(__dirname, "src"),
   testURL: "http://localhost",
   collectCoverageFrom: ["src/**/*.+(js|jsx|ts|tsx)"],
   coverageDirectory: "<rootDir>/coverage",
   moduleFileExtensions: ["js", "jsx", "json", "ts", "tsx"],
   testPathIgnorePatterns: ["/node_modules/"],
   testMatch: ["**/__tests__/**/*.+(js|jsx|ts|tsx)"],
-  transformIgnorePatterns: ["/node_modules/"],
+  transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
   moduleDirectories: ["node_modules", "src"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/src",
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
   },
   extensionsToTreatAsEsm: [".ts", ".tsx"],
 };
