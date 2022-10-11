bc6243d5b079722e22dafdc4e362c0ec68463bc3
Lin Pei Wen <mobilesuit0922@gmail.com>
2022-10-11 23:31:01 +0800

    feat: test add

--- src/components/HomeContainer/__tests__/DecorateBox.test.tsx
+++ src/components/HomeContainer/__tests__/DecorateBox.test.tsx
@@ -1,12 +1,59 @@
-import { Logo } from "../DecorateBox";
+import DecorateBox, { Logo } from "../DecorateBox";
 import { render, screen } from "@testing-library/react";
 
-test("Render DecorateBox", async () => {
+test("DecorateBox Logo render", async () => {
   const { container } = render(<Logo />);
-  screen.debug(container);
-  // screen.getByRole('img', {
-  //   name: /裝飾標題相片/i
-  // })
 
-  // expect(container).toMatchInlineSnapshot();
+  expect(container).toMatchInlineSnapshot(`
+<div>
+  <div
+    class="todo-flex todo-justify-center todo-items-center todo-text-center"
+  >
+    <img
+      alt="裝飾標題相片"
+      class="todo-inline-block"
+      src="test-file-stub"
+    />
+    <img
+      alt="裝飾標題相片"
+      class="todo-inline-block"
+      src="test-file-stub"
+    />
+  </div>
+</div>
+`);
 });
+
+test("DecorateBox render", async () => {
+  const { container } = render(<DecorateBox />);
+
+  expect(container).toMatchInlineSnapshot(`
+<div>
+  <div
+    class="todo-block sm:todo-flex sm:todo-flex-col sm:todo-content-center sm:todo-justify-center"
+  >
+    <div
+      class="todo-flex todo-justify-center todo-items-center todo-text-center"
+    >
+      <img
+        alt="裝飾標題相片"
+        class="todo-inline-block"
+        src="test-file-stub"
+      />
+      <img
+        alt="裝飾標題相片"
+        class="todo-inline-block"
+        src="test-file-stub"
+      />
+    </div>
+    <img
+      alt="裝飾人物相片"
+      class="todo-hidden sm:todo-block todo-mx-auto todo-mt-4"
+      src="test-file-stub"
+    />
+  </div>
+</div>
+`);
+});
+
+test("Login error", async () => {});
---
+++ src/pages/Home/Login/__tests__/Login.test.tsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import Login from "../index";
+// import { render, screen } from "@testing-library/react";
+
+// test("Login error", async () => {
+//   const { container } = render(<Login />);
+// })
